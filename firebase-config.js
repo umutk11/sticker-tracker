0c80f65a62befbb19d1d3465e858cf413957d67f
umutk11 <102241642+umutk11@users.noreply.github.com>
2026-05-17 12:48:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDTBJQJpaNOUp_y0FPFBjkpr_HO5UhNZhM",
   authDomain: "sticker-tracker-c487a.firebaseapp.com",
   projectId: "sticker-tracker-c487a",
@@ -7,3 +15,7 @@ export const firebaseConfig = {
   appId: "1:1085731444908:web:6156ae69bb64322c3f2709",
   measurementId: "G-LG2TGV91ZB"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
