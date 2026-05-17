eedcf6838b290e8b2424b2a03b2301871e97cf85
umutk11 <102241642+umutk11@users.noreply.github.com>
2026-05-17 11:43:07 +0300

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDTBJQJpaNOUp_y0FPFBjkpr_HO5UhNZhM",
+  authDomain: "sticker-tracker-c487a.firebaseapp.com",
+  projectId: "sticker-tracker-c487a",
+  storageBucket: "sticker-tracker-c487a.firebasestorage.app",
+  messagingSenderId: "1085731444908",
+  appId: "1:1085731444908:web:6156ae69bb64322c3f2709",
+  measurementId: "G-LG2TGV91ZB"
+};
